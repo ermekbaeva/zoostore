950e685ded15dbab99af48e4981f845554354fd7
ermekbaeva <ermekbaevameerim@gmail.com>
2024-09-09 19:14:07 +0600

    images in carousel were changed, goods in sale (main page) were changed

--- main/views.py
+++ main/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from goods.models import Categories, Subcategories
+from goods.models import Categories, Subcategories, Products
+import random
 
 def index(request):
     categories = Categories.objects.all()
     subcategories = Subcategories.objects.all()
+    discounted_products = Products.objects.filter(discount__gt=0)
+    
+    discounted_products = random.sample(list(discounted_products), min(len(discounted_products), 12))
     context={
         'title':'Home',
         'content':'Main page',
         'categories':categories,
-        'subcategories':subcategories
+        'subcategories':subcategories,
+        'goods':discounted_products,
     }
     return render(request, 'main/index.html', context)
 
