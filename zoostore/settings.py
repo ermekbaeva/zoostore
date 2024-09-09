938cded1e167beffe4eb5f8df5f8d99c6f9fcacd
ermekbaeva <ermekbaevameerim@gmail.com>
2024-09-09 19:57:36 +0600

    debug mode off

--- zoostore/settings.py
+++ zoostore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-y^rre9#e5d75^^hybjfdsvwmr+cn_g+kapp!qc77)l6x#%+qa5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
