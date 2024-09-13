882857131d0c1cba2bcc7215bb53ae6991cb2efd
ermekbaeva <ermekbaevameerim@gmail.com>
2024-09-13 15:38:50 +0600

    replace database to sqlite file

--- zoostore/settings.py
+++ zoostore/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = "zoostore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "petshopdb",
-        "USER": "petshop",
-        "PASSWORD": "petshop43046755@",
-        "HOST": "localhost",
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
